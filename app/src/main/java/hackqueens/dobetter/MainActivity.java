e2c4e11bc951966db1a97a208a2a296010901285
hlgreer <hannahgreer@rogers.com>
2017-02-04 13:27:07 -0500

    Created Repo

---
+++ app/src/main/java/hackqueens/dobetter/MainActivity.java
@@ -0,0 +1,13 @@
+package hackqueens.dobetter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
